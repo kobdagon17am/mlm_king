77102e226b9a8008a64f3d6ee9c73f3e25fa234d
sassanapong <sassanapong.p@gmail.com>
2023-06-19 23:20:30 +0700

    Bow_order

--- local/resources/views/frontend/order.blade.php
+++ local/resources/views/frontend/order.blade.php
@@ -5,77 +5,81 @@
     <link href="{{ asset('frontend/assets/css/ui-elements/pagination.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 @section('content')
-
     <!--  Main Container Starts  -->
 
-        <!--  Content Area Starts  -->
-        <div id="content" class="main-content">
-            <!--  Navbar Starts / Breadcrumb Area  -->
-            <div class="sub-header-container">
-                <header class="header navbar navbar-expand-sm">
-                    <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom">
-                        <i class="las la-bars"></i>
-                    </a>
-                    <ul class="navbar-nav flex-row">
-                        <li>
-                            <div class="page-header">
-                                <nav class="breadcrumb-one" aria-label="breadcrumb">
-                                    <ol class="breadcrumb">
-                                        <li class="breadcrumb-item active" aria-current="page">
-                                            <span>ประวัติการสั่งซื้อ</span>
-                                        </li>
-                                    </ol>
-                                </nav>
-                            </div>
-                        </li>
-                    </ul>
-                </header>
-            </div>
-            <!--  Navbar Ends / Breadcrumb Area  -->
-            <!-- Main Body Starts -->
+    <!--  Content Area Starts  -->
+    <div id="content" class="main-content">
+        <!--  Navbar Starts / Breadcrumb Area  -->
+        <div class="sub-header-container">
+            <header class="header navbar navbar-expand-sm">
+                <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom">
+                    <i class="las la-bars"></i>
+                </a>
+                <ul class="navbar-nav flex-row">
+                    <li>
+                        <div class="page-header">
+                            <nav class="breadcrumb-one" aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item active" aria-current="page">
+                                        <span>ประวัติการสั่งซื้อ</span>
+                                    </li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </li>
+                </ul>
+            </header>
+        </div>
+        <!--  Navbar Ends / Breadcrumb Area  -->
+        <!-- Main Body Starts -->
 
 
 
-            <div class="layout-px-spacing">
-                <div class="row layout-top-spacing">
-                    <div class="col-lg-12 layout-spacing">
-                        <div class="statbox widget box box-shadow mb-4">
-                            <div class="widget-content widget-content-area">
+        <div class="layout-px-spacing">
+            <div class="row layout-top-spacing">
+                <div class="col-lg-12 layout-spacing">
+                    <div class="statbox widget box box-shadow mb-4">
+                        <div class="widget-content widget-content-area">
                             <div class="widget-header">
                                 <div class="row">
                                     <div class="col-xl-12 col-md-12 col-sm-12 col-12">
                                         {{-- <h5 class="table-header"><b>รายการสั่งซื้อ</b></h5> --}}
                                         <div class="row">
-                                            <div class="col-md-2">
+                                            <div class="col-md-2 mt-2">
                                                 <div class="form-group">
+                                                    <label><b>สถานะการสั่งซื้อ:</b></label>
                                                     <select class="form-control" id="statusfilter">
-                                                        <option>Pending</option>
-                                                        <option>Success</option>
-                                                        <option>Fail</option>
+                                                        <option>สำเร็จ</option>
+                                                        <option>ไม่สำเร็จ</option>
+                                                        <option>กำลังดำเนินการ</option>
+                                                        <option>ยกเลิก</option>
                                                     </select>
                                                 </div>
                                             </div>
-                                            <div class="col-md-4">
+                                            <div class="col-md-4 mt-2">
                                                 <div class="form-group">
+                                                    <label><b>วันที่สั่งซื้อ:</b></label>
                                                     <input class="form-control" type="date" value="yyyy-mm-dd"
                                                         id="searchdate1"><label for="searchdate1">
                                                 </div>
                                             </div>
-                                            <div class="col-md-4">
-
-                                                <input class="form-control" type="date" value="yyyy-mm-dd"
-                                                    id="searchdate2"><label for="searchdate2">
+                                            <div class="col-md-4 mt-2">
+                                                <div class="form-group">
+                                                    <label><b>วันที่สั่งซื้อ:</b></label>
+                                                    <input class="form-control" type="date" value="yyyy-mm-dd"
+                                                        id="searchdate2"><label for="searchdate2">
+                                                </div>
                                             </div>
-                                            <div class="col-md-2">
+                                            <div class="col-md-2 text-center" style="margin-top:45px">
                                                 <button type="button"
-                                                    class="btn btn-outline-success btn-rounded">FILTER</button>
+                                                    class="btn btn-outline-success btn-rounded"><i class="las la-search font-15"></i> สืบค้น</button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="table-responsive mb-4">
+                            <div class="table-responsive mt-2 mb-4">
                                 <table id="ordertable" class="table table-hover" style="width:100%">
                                     <thead>
                                         <tr>
@@ -86,14 +90,14 @@
                                             <th>ชำระโดย</th>
                                             <th>สถานะ</th>
                                             <th>หมายเลขพัสดุ</th>
-                                            <th>ACTION</th>
+                                            <th>รายละเอียด</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
                                             <td>06/09/2023</td>
                                             <td>BILL001</td>
-                                            <td>฿ 1,550</td>
+                                            <td>฿1,550</td>
                                             <td>200</td>
                                             <td>กิ่งทองใบหยก</td>
                                             <td>
@@ -102,15 +106,15 @@
                                             <td>TRACK001</td>
                                             <td class="text-center">
                                                 <div class="dropdown custom-dropdown">
-                                                    <a class="dropdown-toggle font-20 text-primary"
-                                                        href="view" role="button" data-toggle="dropdown"
-                                                        aria-haspopup="true" aria-expanded="false">
-                                                        <i class="las la-eye"></i>
+                                                    <a class="dropdown-toggle font-20 text-primary" href="view"
+                                                        role="button" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                        <i class="las la-eye text-info"></i>
                                                     </a>
-                                                    <a class="dropdown-toggle font-20 text-primary"
-                                                        href="delete" role="button" data-toggle="dropdown"
-                                                        aria-haspopup="true" aria-expanded="false">
-                                                        <i class="las la-trash-alt"></i>
+                                                    <a class="dropdown-toggle font-20 text-primary" href="delete"
+                                                        role="button" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                        <i class="las la-trash-alt text-danger"></i>
                                                     </a>
                                                 </div>
                                             </td>
@@ -118,7 +122,7 @@
                                         <tr>
                                             <td>06/09/2023</td>
                                             <td>BILL002</td>
-                                            <td>฿ 1,550</td>
+                                            <td>฿1,550</td>
                                             <td>200</td>
                                             <td>กิ่งทองใบหยก</td>
                                             <td>
@@ -127,15 +131,15 @@
                                             <td>TRACK002</td>
                                             <td class="text-center">
                                                 <div class="dropdown custom-dropdown">
-                                                    <a class="dropdown-toggle font-20 text-primary"
-                                                        href="view" role="button" data-toggle="dropdown"
-                                                        aria-haspopup="true" aria-expanded="false">
-                                                        <i class="las la-eye"></i>
+                                                    <a class="dropdown-toggle font-20 text-primary" href="view"
+                                                        role="button" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                        <i class="las la-eye text-info"></i>
                                                     </a>
-                                                    <a class="dropdown-toggle font-20 text-primary"
-                                                        href="delete" role="button" data-toggle="dropdown"
-                                                        aria-haspopup="true" aria-expanded="false">
-                                                        <i class="las la-trash-alt"></i>
+                                                    <a class="dropdown-toggle font-20 text-primary" href="delete"
+                                                        role="button" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                        <i class="las la-trash-alt text-danger"></i>
                                                     </a>
                                                 </div>
                                             </td>
@@ -143,7 +147,7 @@
                                         <tr>
                                             <td>06/09/2023</td>
                                             <td>BILL002</td>
-                                            <td>฿ 1,550</td>
+                                            <td>฿1,550</td>
                                             <td>200</td>
                                             <td>กิ่งทองใบหยก</td>
                                             <td>
@@ -152,15 +156,15 @@
                                             <td>TRACK002</td>
                                             <td class="text-center">
                                                 <div class="dropdown custom-dropdown">
-                                                    <a class="dropdown-toggle font-20 text-primary"
-                                                        href="view" role="button" data-toggle="dropdown"
-                                                        aria-haspopup="true" aria-expanded="false">
-                                                        <i class="las la-eye"></i>
+                                                    <a class="dropdown-toggle font-20 text-primary" href="view"
+                                                        role="button" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                        <i class="las la-eye text-info"></i>
                                                     </a>
-                                                    <a class="dropdown-toggle font-20 text-primary"
-                                                        href="delete" role="button" data-toggle="dropdown"
-                                                        aria-haspopup="true" aria-expanded="false">
-                                                        <i class="las la-trash-alt"></i>
+                                                    <a class="dropdown-toggle font-20 text-primary" href="delete"
+                                                        role="button" data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                        <i class="las la-trash-alt text-danger"></i>
                                                     </a>
                                                 </div>
                                             </td>
@@ -168,7 +172,7 @@
                                         <tr>
                                             <td>06/09/2023</td>
                                             <td>BILL003</td>
-                                            <td>฿ 1,550</td>
+                                            <td>฿1,550</td>
                                             <td>200</td>
                                             <td>กิ่งทองใบหยก</td>
                                             <td>
@@ -177,17 +181,15 @@
                                             <td>TRACK003</td>
                                             <td class="text-center">
                                                 <div class="dropdown custom-dropdown">
-                                                    <a class="dropdown-toggle font-20 text-primary"
-                                                        href="view" role="button"
-                                                        data-toggle="dropdown" aria-haspopup="true"
+                                                    <a class="dropdown-toggle font-20 text-primary" href="view"
+                                                        role="button" data-toggle="dropdown" aria-haspopup="true"
                                                         aria-expanded="false">
-                                                        <i class="las la-eye"></i>
+                                                        <i class="las la-eye text-info"></i>
                                                     </a>
-                                                    <a class="dropdown-toggle font-20 text-primary"
-                                                        href="delete" role="button"
-                                                        data-toggle="dropdown" aria-haspopup="true"
+                                                    <a class="dropdown-toggle font-20 text-primary" href="delete"
+                                                        role="button" data-toggle="dropdown" aria-haspopup="true"
                                                         aria-expanded="false">
-                                                        <i class="las la-trash-alt"></i>
+                                                        <i class="las la-trash-alt text-danger"></i>
                                                     </a>
                                                 </div>
                                             </td>
@@ -198,14 +200,13 @@
                             </div>
 
 
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-    @endsection
-    <!-- Main Container Ends -->
+    </div>
+@endsection
+<!-- Main Container Ends -->
 @section('js')
 @endsection
