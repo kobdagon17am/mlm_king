7294b0e7e0311e582ecc844587066f337247dfbc
Sasanapong <sassanapong.p@gmail.com>
2023-07-09 23:27:50 +0700

    ccccssss

--- local/routes/web-backend.php
+++ local/routes/web-backend.php
@@ -26,10 +26,10 @@ Route::get('admin/Blank', function () {
   // Route::get('admin/Products', function () {
   //   return view('backend.products');
   // })->name('admin/Products');
-  Route::get('admin/Products','admin\ProductsController@index')->name('admin/Products');
-  Route::post('admin/Products_insert','admin\ProductsController@insert')->name('admin/Products_insert');
-  Route::get('admin/view_products','admin\ProductsController@view_products')->name('admin/view_products');
-  Route::post('admin/edit_products','admin\ProductsController@edit_products')->name('admin/edit_products');
+  Route::get('admin/Products','Admin\ProductsController@index')->name('admin/Products');
+  Route::post('admin/Products_insert','Admin\ProductsController@insert')->name('admin/Products_insert');
+  Route::get('admin/view_products','Admin\ProductsController@view_products')->name('admin/view_products');
+  Route::post('admin/edit_products','Admin\ProductsController@edit_products')->name('admin/edit_products');
 
 
   Route::get('admin/EditProfile', function () {
@@ -39,30 +39,29 @@ Route::get('admin/Blank', function () {
   // Route::get('admin/Category', function () {
   //   return view('backend.category');
   // })->name('admin/Category');
-  Route::get('admin/Category','admin\CategoryController@index')->name('admin/Category');
-  Route::post('admin/Category_insert','admin\CategoryController@insert')->name('admin/Category_insert');
-  Route::get('admin/view_category','admin\CategoryController@view_category')->name('admin/view_category');
-  Route::post('admin/edit_category','admin\CategoryController@edit_category')->name('admin/edit_category');
+  Route::get('admin/Category','Admin\CategoryController@index')->name('admin/Category');
+  Route::post('admin/Category_insert','Admin\CategoryController@insert')->name('admin/Category_insert');
+  Route::get('admin/view_category','Admin\CategoryController@view_category')->name('admin/view_category');
+  Route::post('admin/edit_category','Admin\CategoryController@edit_category')->name('admin/edit_category');
 
-  
+
 
 
   // Route::get('admin/Unit', function () {
   //   return view('backend.unit');
   // })->name('admin/Unit');
-  Route::get('admin/Unit','admin\UnitController@index')->name('admin/Unit');
-  Route::post('admin/Unit_insert','admin\UnitController@insert')->name('admin/Unit_insert');
-  Route::get('admin/view_unit','admin\UnitController@view_unit')->name('admin/view_unit');
-  Route::post('admin/edit_unit','admin\UnitController@edit_unit')->name('admin/edit_unit');
+  Route::get('admin/Unit','Admin\UnitController@index')->name('admin/Unit');
+  Route::post('admin/Unit_insert','Admin\UnitController@insert')->name('admin/Unit_insert');
+  Route::get('admin/view_unit','Admin\UnitController@view_unit')->name('admin/view_unit');
+  Route::post('admin/edit_unit','Admin\UnitController@edit_unit')->name('admin/edit_unit');
 
 
   // Route::get('admin/Branch', function () {
   //   return view('backend.branch');
   // })->name('admin/Branch');
-  Route::get('admin/Branch','admin\BranchController@index')->name('admin/Branch');
-  Route::post('admin/Branch_insert','admin\BranchController@insert')->name('admin/Branch_insert');
-  Route::get('admin/view_branch','admin\BranchController@view_branch')->name('admin/view_branch');
-  Route::post('admin/edit_branch','admin\BranchController@edit_branch')->name('admin/edit_branch');
+  Route::get('admin/Branch','Admin\BranchController@index')->name('admin/Branch');
+  Route::post('admin/Branch_insert','Admin\BranchController@insert')->name('admin/Branch_insert');
+  Route::get('admin/view_branch','Admin\BranchController@view_branch')->name('admin/view_branch');
+  Route::post('admin/edit_branch','Admin\BranchController@edit_branch')->name('admin/edit_branch');
 
-  
-  
+
